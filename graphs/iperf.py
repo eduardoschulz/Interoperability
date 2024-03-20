8d654155f251ef4b6c2a68b30210893b91e582d7
gckopper <gabriel@gck.dev.br>
2024-03-20 15:49:15 -0300

    new iperf graph

--- graphs/iperf.py
+++ graphs/iperf.py
@@ -4,29 +4,41 @@ import matplotlib as mpl
 import json
 
 mpl.use('QtAgg') 
-files = ["../logs/iperf-srsran.json", "../logs/iperf-oai.json"]
-labels = [["OAI CN", "Open5Gs"], ["Open5Gs", "OAI CN"]]
-ylim = [(100, 130), (20, 23)]
-titles = ["SRSRAN", "OAI"]
+files = ["../logs/oai/oai/iperf/oai-oaicn",
+         "../logs/oai/open5gs/iperf/oai-open5gs",
+         "../logs/oai/free5gc/iperf/20240304-oai-free5gc",
+         "../logs/srsran/oai/iperf/20240304-oai",
+         "../logs/srsran/open5gs/iperf/20240304",
+         "../logs/srsran/free5gc/iperf/20240304-free5gc"
+]
+labels = [
+        "OAI (OAI CN)",
+        "OAI (Open5Gs)",
+        "OAI (Free5Gc)",
+        "SRSRAN (OAI CN)",
+        "SRSRAN (Open5Gs)",
+        "SRSRAN (Free5Gc)",
+]
 count = len(files)
-fig, ax = plt.subplots(1, count)
+ax = plt.subplot()
 
 def conv(x):
     return float(x['sum']['bits_per_second'])/1_000_000
 
 def from_iter(x):
-    print(x['title'].split('-')[1])
     return np.fromiter(map(conv, x['intervals']), float)
 
-ax[0].set_ylabel("Taxa de transferencia (Mbps)")
+ax.set_ylabel("Taxa de transferencia (Mbps)", fontsize=14)
+ax.set_xlabel("Pares de RAN e núcleo usados no teste", fontsize=14)
 
+dataset = []
 for i in range(count):
     with open(files[i], "r") as file:
         data = json.load(file)
-    rev = filter(lambda x: x['title'].endswith('-rev'),data)
-    boxes = list(map(from_iter, rev))
-    ax[i].boxplot(boxes, labels=labels[i])
-    ax[i].set_ylim(ylim[i])
-    ax[i].set_title(titles[i])
+    dataset.append(list(from_iter(data)))
+
+ax.boxplot(dataset)
+ax.set_xticklabels(labels, fontsize=12)
+ax.set_ylim((0,150))
 
 plt.show()
